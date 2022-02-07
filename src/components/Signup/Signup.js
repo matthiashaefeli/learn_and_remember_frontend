767ce4ddbb9d9d0e4ba0139091a569229798dc08
matthiashaefeli <haefeli.matthias2gmail.com>
2022-02-06 19:00:25 -0600

    start with input file

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
+  state = {
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  handleInput = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  handleSubmit = (e) => {
+    // here we create user with axios
+  }
+
   render() {
     return (
       <div>
-        signup
+        <form>
+          <label>
+            <p>name</p>
+            <input
+              name='name'
+              type="text"
+              onChange={this.handleInput.bind(this)}
+            />
+          </label>
+          <label>
+            <p>email</p>
+            <input
+              name='email'
+              type="text"
+              onChange={this.handleInput.bind(this)}
+            />
+          </label>
+          <label>
+            <p>Password</p>
+            <input
+              name='password'
+              type="password"
+              onChange={this.handleInput.bind(this)}
+            />
+          </label>
+          <div>
+            <button
+              type="submit"
+              onClick={this.handleSubmit()}
+            >Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
