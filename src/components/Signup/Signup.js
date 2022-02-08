1c660de2c9e451d64bc5e453cb8675b6bd7da228
matthiashaefeli <haefeli.matthias2gmail.com>
2022-02-07 19:56:02 -0600

    store token in localstorage

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Signup extends Component {
   state = {
@@ -7,12 +8,39 @@ class Signup extends Component {
     password: ''
   }
 
-  handleInput = (e) => {
+  handleInput(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  handleSubmit = (e) => {
-    // here we create user with axios
+  handleSubmit(event) {
+    event.preventDefault();
+    const { name, email, password } = this.state
+    axios({
+      url: 'http://localhost:3000/graphql',
+      method: 'post',
+      data: {
+        query: `
+          mutation {
+            addUser(input: { params: { name: "${name}", email: "${email}", password: "${password}" }}) {
+              authenticate {
+                token
+              }
+              user {
+                name
+                email
+              }
+            }
+          }
+        `
+      }
+    }).then((result) => {
+      this.setState({ name: '', email: '', password: '' })
+      const { token } = result.data.data.addUser.authenticate
+      const { name, email } = result.data.data.addUser.user
+      localStorage.setItem('token', token)
+      localStorage.setItem('name', name)
+      localStorage.setItem('email', email)
+    })
   }
 
   render() {
@@ -26,6 +54,7 @@ class Signup extends Component {
               name='name'
               type="text"
               onChange={this.handleInput.bind(this)}
+              value={this.state.name}
             />
           </label>
           <label>
@@ -35,6 +64,7 @@ class Signup extends Component {
               name='email'
               type="email"
               onChange={this.handleInput.bind(this)}
+              value={this.state.email}
             />
           </label>
           <label>
@@ -44,12 +74,13 @@ class Signup extends Component {
               name='password'
               type="password"
               onChange={this.handleInput.bind(this)}
+              value={this.state.password}
             />
           </label>
           <div>
             <button
               type="submit"
-              onClick={this.handleSubmit()}
+              onClick={this.handleSubmit.bind(this)}
             >Submit</button>
           </div>
         </form>
