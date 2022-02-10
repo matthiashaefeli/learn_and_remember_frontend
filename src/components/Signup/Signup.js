ec48c63c5a95cc3f2548fa6abc2400a928d1f823
matthiashaefeli <haefeli.matthias2gmail.com>
2022-02-09 19:25:53 -0600

    sign in refactor

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,66 +1,10 @@
 import React, { Component } from 'react';
-import AuthService from '../../services/auth_service';
+import Userform from '../Forms/Userform';
 
 class Signup extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: ''
-  }
-
-  handleInput(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    const { name, email, password } = this.state
-    AuthService.register(name, email, password)
-    this.setState({name: '', email: '', password: ''})
-  }
-
   render() {
     return (
-      <div>
-        <form>
-          <label>
-            <p>Name</p>
-            <input
-              data-testid='name-input'
-              name='name'
-              type="text"
-              onChange={this.handleInput.bind(this)}
-              value={this.state.name}
-            />
-          </label>
-          <label>
-            <p>Email</p>
-            <input
-              data-testid='email-input'
-              name='email'
-              type="email"
-              onChange={this.handleInput.bind(this)}
-              value={this.state.email}
-            />
-          </label>
-          <label>
-            <p>Password</p>
-            <input
-              data-testid='password-input'
-              name='password'
-              type="password"
-              onChange={this.handleInput.bind(this)}
-              value={this.state.password}
-            />
-          </label>
-          <div>
-            <button
-              type="submit"
-              onClick={this.handleSubmit.bind(this)}
-            >Submit</button>
-          </div>
-        </form>
-      </div>
+      <Userform auth='register' />
     );
   }
 }
