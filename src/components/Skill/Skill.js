176cf0bd95092a3bc67bd02aecd787c6e1c3fb1b
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-02-20 13:00:02 -0600

    start with rich text editor (#24)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import { useParams } from "react-router-dom";
-import SkillService from '../../services/skill_service'
-import Comment from '../Comment/Index'
+import SkillService from '../../services/skill_service';
+import Comment from '../Comment/Index';
+import CommentForm from '../Forms/CommentForm';
 
 const Skill = () => {
   const [skill, setSkill] = useState({});
@@ -19,6 +20,7 @@ const Skill = () => {
       <p>Language: {skill?.language?.label}</p>
       <p>User: {skill?.user?.name}</p>
       <p>Comments:</p>
+      <CommentForm />
       {skill?.comments?.map(comment => (
           <Comment key={comment.id} comment={comment} />
         ))}
