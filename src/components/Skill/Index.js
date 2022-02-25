4d6a4622a2f06f857b0a99725c227e8090d28703
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-02-24 18:10:59 -0600

    pagination on skills (#30)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

--- src/components/Skill/Index.js
+++ src/components/Skill/Index.js
@@ -1,50 +1,33 @@
-import axios from 'axios';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Skills from '../Skill/Skills';
-import Loading from '../Loading/Loading';
-import NetworkError from '../Network/Network';
 import SkillService from '../../services/skill_service';
+import {
+  FaLongArrowAltLeft,
+  FaLongArrowAltRight
+} from 'react-icons/fa';
 
-class Index extends Component {
-  constructor(props) {
-    super(props);
+function Index () {
+  const [skills, setSkills] = useState([])
+  const [page, setPage] = useState(1)
 
-    this.state = {
-      error: null,
-      isLoaded: false,
-      skills: [],
-      page: 1
-    }
-  }
-
-  componentDidMount() {
-    SkillService.fetchSkillsByStatus(2, 1).then((response) => {
-        this.setState({
-          isLoaded: true,
-          skills: response
-        })
+  useEffect(() => {
+    SkillService.fetchSkillsByStatus(2, page).then((response) => {
+      setSkills(response)
     })
-  }
+  })
 
-  render() {
-    const { error, isLoaded, skills } = this.state;
-
-    if (error) {
-      return <NetworkError />
-    }
-
-    if (!isLoaded) {
-      return <Loading />
-    }
-
-    return (
+  return (
+    <div>
+      {skills.map(skill => (
+        <Skills key={skill.id} skill={skill} />
+      ))}
       <div>
-        {skills.map(skill => (
-          <Skills key={skill.id} skill={skill} />
-        ))}
+        <button onClick={() => setPage(1)}>First Page</button>
+        <button><FaLongArrowAltLeft onClick={() => {if(page>1) setPage(page - 1)}} /></button>
+        <button><FaLongArrowAltRight onClick={() => {if(skills.length===20) setPage(page + 1)}} /></button>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Index;
