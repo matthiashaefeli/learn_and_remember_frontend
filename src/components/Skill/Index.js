68aa3171c680321078f33d963935f1fdebc768fe
matthiashaefeli <haefeli.matthias2gmail.com>
2022-02-25 12:52:51 -0600

    create comment for skill

--- src/components/Skill/Index.js
+++ src/components/Skill/Index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Skills from '../Skill/Skills';
 import SkillService from '../../services/skill_service';
+import AuthService from '../../services/auth_service';
 import {
   FaLongArrowAltLeft,
   FaLongArrowAltRight
@@ -9,18 +10,27 @@ import {
 function Index () {
   const [skills, setSkills] = useState([])
   const [page, setPage] = useState(1)
+  const [skillStatus, setSkillStatus] = useState(2)
 
   useEffect(() => {
-    SkillService.fetchSkillsByStatus(2, page).then((response) => {
+    SkillService.fetchSkillsByStatus(skillStatus, page).then((response) => {
       setSkills(response)
     })
-  })
+  }, [skillStatus, page])
+
+  function onChange(event) {
+    setSkillStatus(parseInt(event.target.value))
+  }
 
   return (
     <div>
       {skills.map(skill => (
         <Skills key={skill.id} skill={skill} />
       ))}
+      <div>
+        <input type="radio" value="1" name="skillStatus" checked={skillStatus === 1} onChange={onChange} /> Unpublished
+        <input type="radio" value="2" name="skillStatus" checked={skillStatus === 2} onChange={onChange} /> Published
+      </div>
       <div>
         <button onClick={() => setPage(1)}>First Page</button>
         <button><FaLongArrowAltLeft onClick={() => {if(page>1) setPage(page - 1)}} /></button>
