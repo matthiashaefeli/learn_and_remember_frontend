a2c2e984fcd05338059e3a88b1e70562374841a4
Matthias Haefeli <haefeli.matthias@gmail.com>
2022-03-05 10:08:51 -0600

    add user setting query (#44)
    Co-authored-by: matthiashaefeli <haefeli.matthias2gmail.com>

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AuthService from '../../services/auth_service';
+import SettingService from '../../services/setting_service';
 
 function Settings() {
+  const [firstMonth, setFirstMonth] = useState('')
+  const [fiveMonth, setfiveMonth] = useState('')
+  const [year, setYear] = useState('')
+
+  useEffect(() => {
+    const userId = AuthService.getCurrentUserId();
+    SettingService.fetchUserSettings(userId).then((response) => {
+      setFirstMonth(response[0].firstMonth)
+      setfiveMonth(response[0].fiveMonth)
+      setYear(response[0].year)
+    })
+  },[])
+
+function handleChange() {
+
+}
 
   return(
     <div>
-      Settings
+      <div>
+        <label>Send Email after 1 Month:</label>
+        <input
+            type="checkbox"
+            checked={firstMonth}
+            onChange={handleChange}
+          />
+      </div>
+      <div>
+        <label>Send Email after 5 Months:</label>
+        <input
+            type="checkbox"
+            checked={fiveMonth}
+            onChange={handleChange}
+          />
+      </div>
+      <div>
+        <label>Send Email every Year:</label>
+        <input
+            type="checkbox"
+            checked={year}
+            onChange={handleChange}
+          />
+      </div>
     </div>
   )
 }
